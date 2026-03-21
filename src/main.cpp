7d079ffd74655b494f94992b1792fc26ae39003d
Ethan Wolf <Emwolf16@yahoo.com>
2026-03-21 17:31:51 -0500

    lodev ceiling and floor casting added! Next: sprites

--- src/main.cpp
+++ src/main.cpp
@@ -152,6 +152,63 @@ int main()
        
         BeginDrawing();
             ClearBackground(BLACK); 
+
+            //Floot casting
+            for(int y = 0; y < screenHeight; y++){
+                //rayDir for leftmost ray (x = 0) and rightmost ray (x = w)
+                float rayDirX0 = dirX - planeX;
+                float rayDirY0 = dirY - planeY;
+                float rayDirX1 = dirX + planeX;
+                float rayDirY1 = dirY + planeY;
+
+                //current y position compared to the center of the screen ( horizan)
+                int p = y - screenHeight / 2;
+
+                //vertical position of the camera
+                float posZ = 0.5 * screenHeight;
+
+                //Horizontal distance from the camera to the floor for the current row.
+                //0.5 is the z position exactly in the middle betweeen floor and ceiling
+                float rowDistance = posZ / p;
+
+                //calculate real world step vector we have to add for each x (parallel to camera plane)
+                //adding step by step avoids multiplications with a weight  in the inner loop
+                float floorStepX = rowDistance * (rayDirX1 - rayDirX0) / screenWidth;
+                float floorStepY = rowDistance * (rayDirY1 - rayDirY0) / screenWidth;
+
+                //real world coordinates of the leftmost column this will be updated as we step to the right
+                float floorX = posX + rowDistance * rayDirX0;
+                float floorY = posY + rowDistance * rayDirY0;
+
+                for(int x = 0; x < screenWidth; ++x){
+                    //the cell coord is simply got from the integer parts of floorX and floorY
+                    int cellX = (int)(floorX);
+                    int cellY = (int)(floorY);
+
+                    //get the texture coordinates from the fractional part
+                    int tx = (int)(texWidth * (floorX - cellX)) & (texWidth - 1);
+                    int ty = (int)(texHeight * (floorY - cellY)) & (texHeight - 1);
+
+                    floorX += floorStepX;
+                    floorY += floorStepY;
+
+                    //choose texture and draw pixel
+                    int floorTexture = 3;
+                    int ceilingTexture  = 6;
+                    Color floorTexColor = texColors[floorTexture][texWidth * ty + tx];
+                    Color ceilingTexColor = texColors[ceilingTexture][texWidth * ty + tx];
+                    //floor
+                    pixels[(screenHeight - y - 1) * screenWidth + x] = floorTexColor;
+                    //ceiling
+                    pixels[y * screenWidth + x] = ceilingTexColor;
+
+                }
+
+            }
+
+
+
+            //Wall casting
             for(int x = 0; x < screenWidth; x++){
                 //calculate ray position and direction
                 double cameraX = 2 * x / double(screenWidth) - 1; //x-coordinate in camera space
