f8aead30fb5f2c49139b803d9510bbac646d03a4
Ethan Wolf <Emwolf16@yahoo.com>
2026-03-21 11:28:24 -0500

    Implementd lodev untextured raycaster with raylib Next: add textured raycasters code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "raylib.h"
+#include "raymath.h"
+#include <cmath>
 
 #define mapWidth 24
 #define mapHeight 24
@@ -40,6 +42,9 @@ int worldMap[mapWidth][mapHeight] =
 double posX = 22, posY = 12;
 double dirX = -1, dirY = 0;
 double planeX = 0, planeY = 0.66;
+//speed modifiers
+double moveSpeed;//the constant value is in squares/second
+double rotSpeed; //the constant value is in radians/second
 
 int main()
 {
@@ -55,12 +60,48 @@ int main()
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        moveSpeed = GetFrameTime() * 5.0;
+        rotSpeed = GetFrameTime() * 3.0;
         
         // Update
-    
+        //move forware if no wall in front of you
+        if(IsKeyDown(KEY_W)){
+            if(worldMap[int(posX + dirX * moveSpeed)][int(posY)] == 0) posX += dirX * moveSpeed;
+            if(worldMap[int(posX)][int(posY + dirY * moveSpeed)] == 0) posY += dirY * moveSpeed;
+            
+        }
+        //movebackward if no wall behind you
+        if(IsKeyDown(KEY_S)){
+             if(worldMap[int(posX - dirX * moveSpeed)][int(posY)] == 0) posX -= dirX * moveSpeed;
+            if(worldMap[int(posX)][int(posY - dirY * moveSpeed)] == 0) posY -= dirY * moveSpeed;
+        }
+
+        //rotate to the right
+        if(IsKeyDown(KEY_D)){
+            //both camera and direction and camera plane must be rotated
+            double oldDirX = dirX;
+            dirX = dirX * cos(-rotSpeed) - dirY * sin(-rotSpeed);
+            dirY = oldDirX * sin(-rotSpeed) + dirY * cos(-rotSpeed);
+            double oldPlaneX = planeX;
+            planeX = planeX * cos(-rotSpeed) - planeY * sin(-rotSpeed);
+            planeY = oldPlaneX * sin(-rotSpeed) + planeY * cos(-rotSpeed);
+
+        }
+
+        if(IsKeyDown(KEY_A)){
+            //both camera and direction and camera plane must be rotated
+            double oldDirX = dirX;
+            dirX = dirX * cos(rotSpeed) - dirY * sin(rotSpeed);
+            dirY = oldDirX * sin(rotSpeed) + dirY * cos(rotSpeed);
+            double oldPlaneX = planeX;
+            planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
+            planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
+
+        }
         // Draw
        
         BeginDrawing();
+            ClearBackground(BLACK); 
             for(int x = 0; x < screenWidth; x++){
                 //calculate ray position and direction
                 double cameraX = 2 * x / double(screenWidth) - 1; //x-coordinate in camera space
@@ -72,6 +113,91 @@ int main()
                 int mapY = int(posY);
 
                 //length of ray from current postiions to next x or y-side
+                double sideDistX;
+                double sideDistY;
+
+                //length of ray from one x or y-side to the next
+                double deltaDistX = (rayDirX == 0) ? 1e30 : std::abs(1/rayDirX);
+                double deltaDistY = (rayDirY == 0) ? 1e30 : std::abs(1/rayDirY);
+                double perpWallDist;
+
+                //what direction to step in x or y direction (+1 or -1)
+                int stepX;
+                int stepY;
+
+                int hit = 0; //was there a wall hit?
+                int side; //was a NS or a EW wall hit?
+
+                //calculate step and initial sideDist
+                if(rayDirX < 0){
+                    stepX = -1;
+                    sideDistX = (posX - mapX) * deltaDistX;
+                }
+                else{
+                    stepX = 1;
+                    sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+                }
+
+                if(rayDirY < 0){
+                    stepY = -1;
+                    sideDistY = (posY - mapY) * deltaDistY;
+
+                }
+                else{
+                    stepY = 1;
+                    sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+                }
+
+                //start DDA
+                while(hit == 0){
+                    if(sideDistX < sideDistY){
+                        sideDistX += deltaDistX;
+                        mapX += stepX;
+                        side = 0;
+                    }
+                    else{ 
+                        sideDistY += deltaDistY;
+                        mapY += stepY;
+                        side = 1;
+                    }
+
+                    //check for ray hit
+                    if(worldMap[mapX][mapY] > 0) hit = 1;
+                }
+
+                //calculate dist projected on camera direction (Euclidean distance would give the fish eye effect)
+                if(side == 0) perpWallDist = (sideDistX - deltaDistX);
+                else          perpWallDist = (sideDistY - deltaDistY);
+
+                //calculate height of line to draw on screen
+                int lineHeight = (int)(screenHeight / perpWallDist);
+
+                //calculate lowest and highest pixel to fille in current stripe
+                int drawStart = -lineHeight / 2 + screenHeight / 2;
+                if(drawStart < 0) drawStart = 0;
+                int drawEnd = lineHeight / 2 + screenHeight / 2;
+                if(drawEnd >= screenHeight) drawEnd = screenHeight - 1;
+
+                //choose wall color
+                Color color;
+                switch (worldMap[mapX][mapY])
+                {
+                case 1: color = RED; break;
+                case 2: color = GREEN; break;
+                case 3: color = BLUE; break;
+                case 4: color = WHITE; break;
+                                
+                default: color = YELLOW; break;
+                }
+
+                //give x and y sides different brightness
+                if (side == 1) {color = ColorBrightness(color, -0.5f);}
+
+
+
+
+                //draw the pixels of the stripe as a vertical line
+                DrawLine(x, drawStart,x,  drawEnd, color);
             }
             
 
